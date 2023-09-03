1e705894887a1f6efcc0f8d8e2cdcbe8907317e5
JCMartinez <jcmtz.091@gmail.com>
2023-09-02 18:49:04 -0600

    install apexchart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
-import NavigationBar from './components/NavBar/NavigationBar.jsx'
 import ProductsList from './views/Products/List/ProductsList.jsx';
 import ProductsCreate from './views/Products/Create/ProductsCreate.jsx';
 import ProductsDetail from './views/Products/Detail/ProductsDetail.jsx';
-import ProductsUpdate from './views/Products/Update/ProductsUpdate.jsx'
+import ProductsUpdate from './views/Products/Update/ProductsUpdate.jsx';
+import Dashboard from './views/Dashboard/Dashboard.jsx';
 import AdminLogin from './views/AdminLogin/AdminLogin.jsx'
 import './App.css'
 
@@ -12,6 +12,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<AdminLogin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/products" element={<ProductsList />} />
         <Route path="/products/create" element={<ProductsCreate />} />
         <Route path="/products/detail/:id" element={<ProductsDetail />} />

2d3ccc948f1f0e37a2a1fc998d026c85dbaa9e11
gonzalo00913 <gonzalo-masa@outlook.com>
2023-09-04 16:16:25 -0300

    baneo de usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductsList from './views/Products/List/ProductsList.jsx';
 import ProductsCreate from './views/Products/Create/ProductsCreate.jsx';
 import ProductsDetail from './views/Products/Detail/ProductsDetail.jsx';
 import AdminLogin from './views/AdminLogin/AdminLogin.jsx'
+import Users from './views/Users/Users.jsx';
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<AdminLogin />} />
+        <Route path="/users" element={<Users />} />
         <Route path="/products" element={<ProductsList />} />
         <Route path="/products/create" element={<ProductsCreate />} />
         <Route path="/products/detail/:id" element={<ProductsDetail />} />

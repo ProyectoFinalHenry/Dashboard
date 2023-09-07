b9b1ed9e3f8b927d2d93b3797a8a8fdaa994ff8a
unknown <danitelo@gmail.com>
2023-09-07 00:38:17 -0300

    imagen y logout

--- src/components/NavBar/NavigationBar.jsx
+++ src/components/NavBar/NavigationBar.jsx
@@ -71,18 +71,6 @@ const NavigationBar = () => {
                 </NavbarContent>
 
                 <NavbarContent as="div" className="items-center" justify="end">
-                    <Input
-                        classNames={{
-                            base: "max-w-full sm:max-w-[10rem] h-10",
-                            mainWrapper: "h-full",
-                            input: "text-small",
-                            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                        }}
-                        placeholder="Type to search..."
-                        size="sm"
-                        startContent={<SearchIcon size={18} />}
-                        type="search"
-                    />
                     <Dropdown placement="bottom-end">
                         <DropdownTrigger>
                             <Avatar

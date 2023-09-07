27ed3bbecb867cf50138b3b698fec06827436daf
unknown <danitelo@gmail.com>
2023-09-06 23:36:02 -0300

    imagen y logout

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 import axios from "axios";
 import './index.css'
 
-//axios.defaults.baseURL = "http://localhost:3001/";
-axios.defaults.baseURL = "https://backend-mniu.onrender.com/";
+axios.defaults.baseURL = "http://localhost:3001/";
+// axios.defaults.baseURL = "https://backend-mniu.onrender.com/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
